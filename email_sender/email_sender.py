1f88a4595134c21f77a7db5553ccc2d5ef3d2591
Albert Suraliński <albert.suralinski@gmail.com>
2020-05-20 07:05:18 +0200

    Refactoref whole e-mail module.

--- email_sender/email_sender.py
+++ email_sender/email_sender.py
@@ -1,24 +1,27 @@
-import os
+import logging
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
+from pathlib import Path
 from string import Template
 
-from settings import EMAIL_ADDRESSES, EMAIL_FROM, EMAIL_SUBJECT
+from settings import EMAIL_ADDRESSES, EMAIL_FROM, EMAIL_SUBJECT, SMTP_HOST, SMTP_PASSWORD, SMTP_PORT, SMTP_LOGIN
+
+logger = logging.getLogger(__name__)
 
 
 def send_email():
     try:
-        s = smtplib.SMTP(host=os.environ.get('SMTP_HOST'), port=os.environ.get('SMTP_PORT'))
+        s = smtplib.SMTP(host=SMTP_HOST, port=SMTP_PORT)
         s.starttls()
-        s.login(os.environ.get('SMTP_LOGIN'), os.environ.get('SMTP_PASSWORD'))
+        s.login(SMTP_LOGIN, SMTP_PASSWORD)
         for email in EMAIL_ADDRESSES:
             msg = create_msg(email)
             s.send_message(msg)
             del msg
+            logger.debug("E-mail sent.")
     except Exception as e:
-        print(e)
+        logger.error(e)
     finally:
         s.quit()
 
@@ -28,7 +31,7 @@ def create_msg(email_to):
     msg['From'] = EMAIL_FROM
     msg['Subject'] = EMAIL_SUBJECT
     msg['To'] = email_to
-    message = read_template('email_template.html')
+    message = read_template(Path('email_sender/email_template.html'))
     msg.attach(MIMEText(message.template, 'html'))
     return msg
 
--- settings.py
+++ settings.py
@@ -3,13 +3,13 @@ import env
 
 URL = getattr(env, 'URL', None)
 EMAIL_ADDRESSES = getattr(env, 'EMAIL_ADDRESSES', [])
-EMAIL_FROM = getattr(env, 'EMAIL_FROM', None)
+EMAIL_FROM = getattr(env, 'EMAIL_FROM', "Some mail")
 EMAIL_SUBJECT = getattr(env, 'EMAIL_SUBJECT', "Subject")
 
 ORIGINAL_FILE = 'original_content.html'
 FILE_TO_COMPARE = 'content_to_compare.html'
 
-SMTP_HOST = getattr(env, 'SMTP_HOST', "smtp.gmail.com")
+SMTP_HOST = getattr(env, 'SMTP_HOST', None)
 SMTP_PORT = getattr(env, 'SMTP_PORT', "25")
 SMTP_LOGIN = getattr(env, 'SMTP_LOGIN', "login")
 SMTP_PASSWORD = getattr(env, 'SMTP_PASSWORD', "password")
