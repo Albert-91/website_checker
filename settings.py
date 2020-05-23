71441d93af2fff4d7f83f3ee5e4b36e9bc161dcc
Albert Suraliński <albert.suralinski@gmail.com>
2020-05-23 11:07:38 +0200

    Changed names of consts.

--- settings.py
+++ settings.py
@@ -6,8 +6,8 @@ EMAIL_ADDRESSES = getattr(env, 'EMAIL_ADDRESSES', [])
 EMAIL_FROM = getattr(env, 'EMAIL_FROM', "Some mail")
 EMAIL_SUBJECT = getattr(env, 'EMAIL_SUBJECT', "Subject")
 
-ORIGINAL_FILE = 'original_content.html'
-FILE_TO_COMPARE = 'content_to_compare.html'
+ORIGINAL_FILE_NAME = 'original_content.html'
+FILE_TO_COMPARE_NAME = 'content_to_compare.html'
 
 SMTP_HOST = getattr(env, 'SMTP_HOST', None)
 SMTP_PORT = getattr(env, 'SMTP_PORT', "25")
